2d000cc14d08cb089b31533f180f306cbc9fc03c
Nolonwabo <nolonwabomahanjana13@gmail.com>
2017-09-07 15:59:02 +0200

    Storing waiters names per day they selected

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var shiftModel = require('./model');
+// console.log(shiftModel);
 app.use(bodyParser.urlencoded({
   extended: false
 }));
@@ -17,103 +18,124 @@ var massege = '';
 app.get('/waiters/:username', function(req, res, next) {
   var username = req.params.username;
   massege = 'Please select your days ' + username;
-  res.render('index', {username: massege});
+  res.render('index', {
+    username: massege
+  });
   console.log(username);
 });
-var daysSelected = {
-  monday:[],
-  tuesday:[],
-  wensday:[],
-  thursday:[],
-  friday:[],
-  saturday:[],
-  sunday:[]
-};
-app.post('/waiters/:username', function(req, res){
-  var output ='Your shifts has been updated';
+
+// function coloringDays({
+// if()
+// })
+
+app.post('/waiters/:username', function(req, res) {
+  var output = 'Your shifts has been updated';
+  var shift = "Your shift has been added";
   var days = req.body.days;
   var daysObj = {};
-
-  console.log(days);
+  // console.log(days);
   var username = req.params.username;
-  // console.log(username);
-  //for (var i = 0; i < days.length; i++) {
-  //  var currentDay = days[i];
-  //}
-  if(!Array.isArray(days)){
-    days=[days];
+  //convert to array
+  if (!Array.isArray(days)) {
+    days = [days];
   }
-  days.forEach(function(daySelected){
+  //loop thru
+  days.forEach(function(daySelected) {
     daysObj[daySelected] = true
+  })
+  //find 1 and update if username exists
+  shiftModel.findOneAndUpdate({
+      username: username
+    }, {
+      days: daysObj
+    },
+    function(err, waiterName) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (!waiterName) {
+          // create new waiter
+          var storingWaitersNames = new shiftModel({
+            username: username,
+            days: daysObj
+          });
+          //save the new user
+          storingWaitersNames.save(function(err) {
+            console.log('lulo');
+            if (err) {
+              console.log('Error Massage:' + err);
+            } else {
+              res.render('index', {
+                output: shift
+              })
 
-  })
-  var storingWaitersNames = new model({
-    username: username,
-    days: daysObj
-  });
-  storingWaitersNames.save(function(err){
-    if(err){
-      console.log('Error Massage:' + err);
-    }
-    else{
-      console.log('Saved to database');
-    }
-  })
-  res.render('index', {output: output});
+              console.log('Saved to database');
+            }
+          })
+        } else {
+          console.log("========+++++++++" + waiterName.days.Monday);
+          //render the updates
+          res.render('index', {
+            output: output,
+            monday: waiterName.days.Monday,
+            tuesday: waiterName.days.Tuesday,
+            wednesday: waiterName.days.Wednesday,
+            thursday: waiterName.days.Thursday,
+            friday: waiterName.days.Friday,
+            saturday: waiterName.days.Saturday,
+            sunday: waiterName.days.Sunday
+          });
+        }
+      }
+    });
 });
+app.get('/days', function(req, res) {
+  shiftModel.find({}, function(err, avalaibleWaiters) {
+    var Monday = [];
+    var Tuesday = [];
+    var Wednesday = [];
+    var Thursday = [];
+    var Friday = [];
+    var Saturday = [];
+    var Sunday = [];
+    if (err) {
+      return err;
+    } else {
+      for (var i = 0; i < avalaibleWaiters.length; i++) {
+        var daysLoop = avalaibleWaiters[i].days;
+        for (var avalaibleDay in daysLoop) {
 
-app.get('/days', function(req, res){
-  shiftModel.find({}, function(err, names){
-    if(err){
-      console.log(err);
+          if (avalaibleDay === 'Monday') {
+            Monday.push(avalaibleWaiters[i].username)
+          } else if (avalaibleDay === 'Tuesday') {
+            Tuesday.push(avalaibleWaiters[i].username)
+          } else if (avalaibleDay === 'Wednesday') {
+            Wednesday.push(avalaibleWaiters[i].username)
+          } else if (avalaibleDay === 'Thursday') {
+            Thursday.push(avalaibleWaiters[i].username)
+          } else if (avalaibleDay === 'Friday') {
+            Friday.push(avalaibleWaiters[i].username)
+          } else if (avalaibleDay === 'Saturday') {
+            Saturday.push(avalaibleWaiters[i].username)
+          } else if (avalaibleDay === 'Sunday') {
+            Sunday.push(avalaibleWaiters[i].username)
+          }
+        }
+      }
     }
-    else{
-      res.render('waiter',{waiter: daysObj})
-    }
-  })
 
+    res.render('waiter', {
+      monday: Monday,
+      tuesday: Tuesday,
+      wednesday: Wednesday,
+      thursday: Thursday,
+      friday: Friday,
+      saturday: Saturday,
+      sunday: Sunday
+    });
+  });
 })
-
 var port = process.env.PORT || 3002
 var server = app.listen(port, function() {
   console.log("Started app on port : " + port)
 });
-
-//
-// model.storeRegNum.findOne({
-//     regNum: regNum
-//   },
-//   function(err, platesStored) {
-//     if (err) {
-//       return (err);
-//     } else if (platesStored) {
-//       storeRegistration.push(platesStored);
-//       platesStored = 1;
-//       res.render('index', {
-//         massege: massage
-//       });
-//     } else {
-//
-//       if (!platesStored) {
-//         var storingRegPlates = new model.storeRegNum({
-//           regNum: regNum
-//         });
-//         storeRegistration.push(platesStored);
-//         storingRegPlates.save(function(err, data) {
-//           if (err) {
-//             return err
-//           }
-//           model.storeRegNum.find({}, function(err, results) {
-//             if (err) {
-//               return err
-//             }
-//             res.render('index', {
-//               numberPlates: results
-//             });
-//
-//           })
-//         })
-//       }
-//     }
-//   })
-// });
--- model.js
+++ model.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 const MongoURL =process.env.MONGO_DB_URL || "mongodb://localhost/waiters";
+console.log(MongoURL);
 mongoose.connect(MongoURL, {
   useMongoClient: true
 });
